e5701b939eb91f524259997639910dd7bcdaa99f
Jvr <109031036+Jvr2022@users.noreply.github.com>
2024-02-10 12:23:18 +0100

    cleanup

--- app/src/main/kotlin/com/apkupdater/application/App.kt
+++ app/src/main/kotlin/com/apkupdater/application/App.kt
@@ -3,31 +3,29 @@ package com.apkupdater.application
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
-import coil.util.DebugLogger
 import com.apkupdater.di.mainModule
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.get
 import org.koin.core.context.startKoin
 
 class App : Application(), ImageLoaderFactory, KoinComponent {
 
-	override fun onCreate() {
-		super.onCreate()
+    override fun onCreate() {
+        super.onCreate()
 
-		startKoin {
-			androidLogger()
-			androidContext(this@App)
-			modules(mainModule)
-		}
-	}
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(mainModule)
+        }
+    }
 
-	override fun newImageLoader() = ImageLoader
-		.Builder(this)
-		.okHttpClient(get<OkHttpClient>())
-		//.logger(DebugLogger())
-		.build()
-
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .okHttpClient(get<OkHttpClient>())
+            //.logger(DebugLogger())
+            .build()
+    }
 }
